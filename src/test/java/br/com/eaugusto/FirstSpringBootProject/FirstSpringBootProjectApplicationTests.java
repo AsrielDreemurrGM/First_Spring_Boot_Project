1b8abcf486ced4034aaeab3ddf6a45762b2dc7c6
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-08-06 10:19:01 -0300

    Created First Project With Spring Boot Initializr
    - From "https://start.spring.io/";
    - Created .gitignore;
    - Added Spring Actuator, Data JPA, Web, Dev Tools, PostgreSQL And Lombok Dependencies;

---
+++ src/test/java/br/com/eaugusto/FirstSpringBootProject/FirstSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eaugusto.FirstSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
